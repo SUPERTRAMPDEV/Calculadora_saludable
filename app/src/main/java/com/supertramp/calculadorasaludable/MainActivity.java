4ef0a83c371c59b447480dcbbddc96fea79a0271
Gabriel Supertramp <Gabintosh1@gmail.com>
2018-04-01 20:06:05 -0500

    Main activity semi-completo

--- app/src/main/java/com/supertramp/calculadorasaludable/MainActivity.java
+++ app/src/main/java/com/supertramp/calculadorasaludable/MainActivity.java
@@ -7,11 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button registros_anteriores,ok;
-    EditText etnombre, etedad,etpeso,altura;
+   private Button registros_anteriores,ok;
+   private EditText etnombre, etedad,etpeso,etaltura;
+   private RadioGroup radioGroupsexo, radioGroupAf;
+   private RadioButton sr,afr;
+   private String nombre,edad,peso,altura,sexo,af;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         registros_anteriores = (Button) findViewById(R.id.registros_anteriores);
+        ok = (Button) findViewById(R.id.boton);
+        etnombre = (EditText) findViewById(R.id.et_nombre);
+        etedad = (EditText) findViewById(R.id.et_edad);
+        etpeso = (EditText) findViewById(R.id.et_peso);
+        etaltura = (EditText) findViewById(R.id.et_altura);
+        radioGroupsexo = (RadioGroup) findViewById(R.id.radiogroupsex);
+        radioGroupAf = (RadioGroup) findViewById(R.id.radiogroupaf);
+
 
         SharedPreferences preferences = getSharedPreferences("config", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
@@ -28,6 +43,25 @@ public class MainActivity extends AppCompatActivity {
         if(datos_existes == null){
             registros_anteriores.setVisibility(View.GONE);
         }
+
+
+
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sr = (RadioButton) findViewById(radioGroupsexo.getCheckedRadioButtonId());
+                afr = (RadioButton) findViewById(radioGroupAf.getCheckedRadioButtonId());
+                nombre = etnombre.getText().toString();
+                edad = etedad.getText().toString();
+                peso = etpeso.getText().toString();
+                altura = etaltura.getText().toString();
+                sexo = sr.getText().toString();
+                af = afr.getText().toString();
+
+
+
+            }
+        });
 
 
     }
