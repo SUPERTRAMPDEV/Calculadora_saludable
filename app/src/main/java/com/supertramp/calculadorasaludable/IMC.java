6adbcb3b3e7b6115b6cd74054565125f59ce6356
Gabriel Supertramp <Gabintosh1@gmail.com>
2018-04-01 20:39:39 -0500

    IMC class
    the class to get the IMC

--- app/src/main/java/com/supertramp/calculadorasaludable/IMC.java
+++ app/src/main/java/com/supertramp/calculadorasaludable/IMC.java
@@ -1,4 +1,17 @@
 package com.supertramp.calculadorasaludable;
 
 public class IMC {
+    private int peso;
+    private int altura;
+
+    public IMC(int peso, int altura){
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int imc(){
+     int result = (int) (this.peso / (Math.pow((this.altura/100),2)));
+     return result;
+    }
+
 }
