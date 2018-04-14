0fae66d3ada0b64e5f0466e25952cb032dac16a8
Gabriel Supertramp <Gabintosh1@gmail.com>
2018-04-14 00:07:02 -0500

    TMB
    tmb

--- app/src/main/java/com/supertramp/calculadorasaludable/TMB.java
+++ app/src/main/java/com/supertramp/calculadorasaludable/TMB.java
@@ -1,4 +1,21 @@
 package com.supertramp.calculadorasaludable;
 
-public class TMB {
+public abstract class TMB {
+    protected int peso;
+    protected int altura;
+    protected int edad;
+
+    public TMB(int peso, int altura, int edad){
+        this.peso = peso;
+        this.altura = altura;
+        this.edad = edad;
+
+    }
+
+    public float simpleTMB(){
+        return Float.parseFloat(String.valueOf((10*peso) + (6.25*altura) - (5*edad)));
+
+    }
+
+
 }
